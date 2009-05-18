eb580b09d1f931a32929c64494dd53765eabadf8
DVSBSTD <kristjanrang@gmail.com>
2009-05-19 00:13:39 +0300

    first commit

---
+++ YouDown/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace YouDown
+{
+    public partial class App : Application
+    {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+        }
+    }
+}
